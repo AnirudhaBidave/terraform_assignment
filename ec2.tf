cfe07942d92b2d1f72645abc93e2e6826a97b762
Anirudha Bidave <130437908+AnirudhaBidave@users.noreply.github.com>
2023-12-01 17:04:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 #ec2
 
 provider "aws" {
-  region  = "us-west-1"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "public_instance" {
-  ami                    = "ami-0f03c2e51408a636c"
+  ami                    = "ami-06c40dd4f80434809"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
@@ -36,7 +36,7 @@ resource "aws_instance" "public_instance" {
 }
 
 resource "aws_instance" "private_instance" {
-  ami                    = "ami-0f03c2e51408a636c"
+  ami                    = "ami-06c40dd4f80434809"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
